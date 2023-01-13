8a3b2639b4a8ceae69bdd52340fd44aa3529a710
Z-n-o-M <41215398+Z-n-o-M@users.noreply.github.com>
2023-01-13 09:12:50 +0200

    docs(example): rephrasing

--- example/dist/bundle.js
+++ example/dist/bundle.js
@@ -7,15 +7,21 @@ class Ox0 {
   }
 }
 
-const secret = 123456;
+const secret = "123456";
 const node_modules = "node_modules";
 
 function main () {
   const Ox3 = new Ox0();
   const Ox4 = Ox3.Ox5(2, 3);
+
+  /* 123456 -> 123456 : defined in predefinedWords inside rollup.config.js */
   console.log("secret: ", secret);
+
+  /* node_modules -> node_modules : defined in ignoredWords inside rollup.config.js */
+  console.log("node_modules:", node_modules);
+
+  /* Ox4 -> Ox4: with prefix l_ will be obfuscated */
   console.log("results: ", Ox4);
-  console.log("node_modules:", node_modules, "(should stay the same) (added to ignoredWords)");
 }
 
 module.exports = main;
--- example/src/main.js
+++ example/src/main.js
@@ -1,12 +1,18 @@
 import { c_Calculator } from "./calculator";
 
-const secret = predefined_secretword;
+const secret = "predefined_secretword";
 const node_modules = "node_modules";
 
 export default function () {
   const l_calculator = new c_Calculator();
   const l_results = l_calculator.f_sum(2, 3);
+
+  /* predefined_secretword -> 123456 : defined in predefinedWords inside rollup.config.js */
   console.log("secret: ", secret);
+
+  /* node_modules -> node_modules : defined in ignoredWords inside rollup.config.js */
+  console.log("node_modules:", node_modules);
+
+  /* l_results -> Ox4: with prefix l_ will be obfuscated */
   console.log("results: ", l_results);
-  console.log("node_modules:", node_modules, "should stay 'node_modules' because it was added to 'ignoredWords' in rollup.config.js");
 }
