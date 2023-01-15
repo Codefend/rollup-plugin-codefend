6c54afe53449f057f9daf897e88752cc6d8f0d91
Z-n-o-M <mounzer.dev@gmail.com>
2023-01-15 15:16:55 +0200

    chore(lib): remove

--- lib/cjs/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { ICodefendRollupPluginOptions } from "./models";
-export declare const codefend: (options: ICodefendRollupPluginOptions) => {
-    name: string;
-    transform(code: string): string;
-};
-//# sourceMappingURL=index.d.ts.map
