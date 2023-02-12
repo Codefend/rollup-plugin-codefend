99fe83bc9ecc66655f597ad49eba0d9d50a62712
Z-n-o-M <mounzer.dev@gmail.com>
2023-02-12 21:48:56 +0200

    fix(stats): stats not displaying at the end

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { OptionsBuilder } from "./options/builder";
-import { buildRuntimeOptions, obfuscate } from "codefend";
+import { buildRuntimeOptions, obfuscate, stats } from "codefend";
 import { IObfuscationOptions } from "codefend/build/src/core/options";
 
 export const codefend = (options: IObfuscationOptions) => {
@@ -11,6 +11,9 @@ export const codefend = (options: IObfuscationOptions) => {
     name: name,
     transform(code: string) {
       return obfuscate(code, _options, runtimeOptions);
+    },
+    generateBundle: function write() {
+      stats({ stats: _options.stats }, runtimeOptions);
     },
   };
 };
